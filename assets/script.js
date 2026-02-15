f306376c58a996e7d7492e8e438be7f3162ff8b6
Navin Chandra <chandranavin042@gmail.com>
2026-02-15 20:36:00 +0530

    add modal to contact from

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+root {
+  --retro-dark: #2d2d2d;
+  --retro-grey: #3d3d3d;
+  --retro-light: #f7f7f7;
+  --retro-red: #d32f2f;
+  --retro-white: #ffffff;
+  --retro-shadow: rgba(42, 42, 42, 0.3);
+  --retro-letter-spacing: 2px;
+};
