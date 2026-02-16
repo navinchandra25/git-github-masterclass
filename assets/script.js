59c1530363e5dcc6e5ecbe752aaf5725ada6cc2b
Navin Chandra <chandranavin042@gmail.com>
2026-02-16 23:44:22 +0530

    revert model to contact from

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-root {
-  --retro-dark: #2d2d2d;
-  --retro-grey: #3d3d3d;
-  --retro-light: #f7f7f7;
-  --retro-red: #d32f2f;
-  --retro-white: #ffffff;
-  --retro-shadow: rgba(42, 42, 42, 0.3);
-  --retro-letter-spacing: 2px;
-};
